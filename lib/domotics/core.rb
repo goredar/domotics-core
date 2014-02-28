2a5ec92636cd05ce935929746513cea95b89c301
goredar <info@goredar.it>
2014-02-28 20:13:33 +0300

    init

---
+++ lib/domotics/core.rb
@@ -0,0 +1,7 @@
+require "domotics/core/version"
+
+module Domotics
+  module Core
+    # Your code goes here...
+  end
+end
---
+++ lib/domotics/core/version.rb
@@ -0,0 +1,5 @@
+module Domotics
+  module Core
+    VERSION = "0.0.1"
+  end
+end
